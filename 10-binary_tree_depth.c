d19d0352382021c5f3c32d760a5b52b42bddeb84
John Olekwa <62917634+olekwa@users.noreply.github.com>
2023-11-02 10:41:15 +0100

    Updated

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
- * binary_tree_depth - depth of specified node from root
- * @tree: node to check the depth
+ * binary_tree_depth - The depth of node from root
+ * @tree: This is the node that check the depth
  * Return: 0 is it is the root or number of depth
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
