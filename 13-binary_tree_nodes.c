954eca013cd5f6849b4d1da4ca2f30804c5859a7
John Olekwa <62917634+olekwa@users.noreply.github.com>
2023-11-02 13:22:07 +0100

    Updated

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
- * binary_tree_nodes - function that returns # of nodes in a tree with children
- * @tree: tree to check
- * Return: number of nodes with children
+ * binary_tree_nodes - The function that returns of nodes in a tree with children
+ * @tree: The tree to checked
+ * Return: no of nodes with children
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
