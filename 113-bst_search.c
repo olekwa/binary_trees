9ecc0152bff59fff859003084e1002c7ccae5217
John Olekwa <62917634+olekwa@users.noreply.github.com>
2023-11-02 11:37:22 +0100

    Updated

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
- * bst_search - search for a special value in the tree and return the node
- * @tree: tree to go through
- * @value: value to search
+ * bst_search - This searches for a value in the tree and return the node
+ * @tree: The tree to be searched
+ * @value: The value to be searched for
  * Return: the node with the value or NULL if the value is not in the tree
  */
 bst_t *bst_search(const bst_t *tree, int value)
